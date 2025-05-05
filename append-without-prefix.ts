5e75511d6c1aa9496f841707174f81e977cc1a60
Magnus Ekse <380919+eksepsjon@users.noreply.github.com>
2025-05-05 10:40:10 +0200

    Added spacing vars

--- append-without-prefix.ts
+++ append-without-prefix.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 
 const inputFiles = ['dist/kystverket.css'];
+const spacingCss = fs.readFileSync('spacing.css', 'utf-8');
 
 const mainFn = async (inputFile) => {
   const css = fs.readFileSync(inputFile, 'utf-8');
@@ -13,6 +14,8 @@ const mainFn = async (inputFile) => {
       outLines.push(newLine);
     }
   });
+
+  outLines.push(spacingCss);
 
   fs.writeFileSync(inputFile, outLines.join('\n'), 'utf-8');
 };

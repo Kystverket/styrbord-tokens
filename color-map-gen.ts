6583654171477a94eb3cbd54b12bab9757169d0d
Magnus Ekse <380919+eksepsjon@users.noreply.github.com>
2025-05-06 09:51:59 +0200

    Lage en static side som viser farger

---
+++ color-map-gen.ts
@@ -0,0 +1,81 @@
+import fs from 'fs';
+import { stringify } from 'querystring';
+
+type ColorStrip = {
+  name: string;
+  colors: string[];
+};
+type ColorMap = {
+  file: string;
+  colors: ColorStrip[];
+};
+
+const inputFiles = [
+  'design-tokens/primitives/modes/color-scheme/dark/global.json',
+  'design-tokens/primitives/modes/color-scheme/dark/kystverket.json',
+  'design-tokens/primitives/modes/color-scheme/light/global.json',
+  'design-tokens/primitives/modes/color-scheme/light/kystverket.json',
+];
+
+const createColorMap = (inputFile): ColorMap => {
+  const colors = JSON.parse(fs.readFileSync(inputFile, 'utf-8'));
+  const outLines: string[] = [];
+
+  const m: ColorMap = {
+    file: inputFile,
+    colors: [],
+  };
+
+  Object.entries(colors).forEach(([collectionName, colorCollection]) => {
+    Object.entries(colorCollection).forEach(([stripName, strip]) => {
+      const colorStrip: ColorStrip = {
+        name: stripName,
+        colors: [],
+      };
+      for (let i = 0; i < 16; i++) {
+        const color = strip[String(i)];
+        if (color) {
+          colorStrip.colors.push(color['$value']);
+        }
+      }
+      m.colors.push(colorStrip);
+    });
+  });
+
+  return m;
+};
+
+const colorMapToHtml = (colorMap: ColorMap): string => {
+  const outLines: string[] = [];
+  outLines.push('<div>');
+  outLines.push('<h2>' + colorMap.file + '</h2>');
+  outLines.push('<div style="display: flex; flex-wrap: wrap;">');
+  colorMap.colors.forEach((colorStrip) => {
+    outLines.push('<div>');
+    outLines.push('<h3>' + colorStrip.name + '</h3>');
+    outLines.push('<table>');
+    colorStrip.colors.forEach((color) => {
+      outLines.push('<tr>');
+      outLines.push(
+        '<td style="width: 128px; height: 32px; background-color: ' + color + '; border: 1px solid #000;"></td>',
+      );
+      outLines.push('<td style="padding: 8px 16px; font-family: monospace;">' + color + '</td>');
+      outLines.push('</tr>');
+    });
+    outLines.push('</table>');
+    outLines.push('</div>');
+  });
+  outLines.push('</div>');
+  outLines.push('</div>');
+  return outLines.join('\n');
+};
+
+const colorMaps: ColorMap[] = inputFiles.map((inputFile) => createColorMap(inputFile));
+
+const endHtml =
+  '<html><head><link href="https://cdn.jsdelivr.net/npm/skeleton-css@2.0.4/css/normalize.min.css" rel="stylesheet"></head><body style="padding: 16px;"><h1>Farger i styrbord-tokens</h1>' +
+  colorMaps.map((cm) => colorMapToHtml(cm)) +
+  '</body></html>';
+
+fs.mkdirSync('static', { recursive: true });
+fs.writeFileSync('static/index.html', endHtml, 'utf-8');
